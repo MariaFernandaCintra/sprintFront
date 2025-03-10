53777fbcf003a785edbed54c38b12567814e8463
mariajuliacintra <mariajuliacintra31@gmail.com>
2025-03-10 16:21:53 -0300

    atualização da tabela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,6 +61,7 @@ function getStyles() {
     header: {
       backgroundColor: "rgba(177, 16, 16, 1)",
       width: "210vh",
+      length:"100%",
       height: "11vh",
       display: "flex",
       alignItems: "center",
