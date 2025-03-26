d8f3eeaea9f9d154048d9b5adbf2cb9277391005
fogazza <vinicius.cintra2801@gmail.com>
2025-03-26 13:51:13 -0300

    26-03-25 / alteração de fonte

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ProtectedRouter from "./components/ProtectedRoute";
 import Cadastro from "./pages/Cadastro";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Principal from "./pages/Principal";
 import Reserva from "./pages/Reserva";
+import "@fontsource/roboto-mono";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto Mono, monospace",
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route
-          path="/principal"
-          element={
-            <ProtectedRouter>
-              <Principal />
-            </ProtectedRouter>
-          }
-        />
-        <Route
-          path="/reserva"
-          element={
-            <ProtectedRouter>
-              <Reserva />
-            </ProtectedRouter>
-          }
-        />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route
+            path="/principal"
+            element={
+              <ProtectedRouter>
+                <Principal />
+              </ProtectedRouter>
+            }
+          />
+          <Route
+            path="/reserva"
+            element={
+              <ProtectedRouter>
+                <Reserva />
+              </ProtectedRouter>
+            }
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
